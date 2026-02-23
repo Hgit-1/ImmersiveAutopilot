7b61ab2e4f94013a7aa00b34f761b79aeec9d382
H <106968861+Hgit-1@users.noreply.github.com>
2026-02-23 19:09:29 +0800

    Fix client event subscriber annotation

--- src/main/java/com/immersiveautopilot/client/ClientEvents.java
+++ src/main/java/com/immersiveautopilot/client/ClientEvents.java
@@ -2,14 +2,13 @@ package com.immersiveautopilot.client;
 
 import com.immersiveautopilot.ImmersiveAutopilot;
 import com.immersiveautopilot.menu.ModMenus;
-import com.immersiveautopilot.screen.RouteOfferScreen;
 import com.immersiveautopilot.screen.TowerScreen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@Mod.EventBusSubscriber(modid = ImmersiveAutopilot.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = ImmersiveAutopilot.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ClientEvents {
     private ClientEvents() {
     }
